f553952d1dbf686689cee272262a43fac550899d
Martin Atkins <mart@degeneration.co.uk>
2012-05-27 08:01:54 -0700

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name="jsonjinja",
+    version="0.1",
+    description="A Jinja2 inspired templating language for rendering JSON data on the server and client.",
+    author="Armin Ronacher",
+    author_email="armin.ronacher@active-4.com",
+    packages=['jsonjinja'],
+)
